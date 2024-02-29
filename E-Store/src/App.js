e71c28f36dada9e076cd78ceea7f8948ec57312e
swati-garule <161654562+swati-garule@users.noreply.github.com>
2024-02-29 06:12:27 +0530

    Add files via upload

---
+++ E-Store/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Books from './Components/Books';
+import "./style.css"
+function App() {
+  return (
+    <div className="App">
+        <Books />
+    </div>
+  );
+}
+
+export default App;
